0209ee7ff4d5e0fdc7b61bf63f5b5fdcdd5d451b
GH057M4N <ivanchox0@gmail.com>
2018-06-03 14:31:41 -0400

    Add files via upload

---
+++ db/conexion.php
@@ -0,0 +1,10 @@
+<?php 
+
+$host = "localhost" ;
+
+$user = "root" ;
+
+$db = "curso php";
+
+
+ ?>
---
+++ db/insertar.php
@@ -0,0 +1,18 @@
+<?php 
+include ("conexion.php");
+if ( isset ( $_POST [ 'nombre' ] ) && !empty ( $_POST [ 'nombre' ] ) &&
+ isset( $_POST [ 'pw' ] ) && !empty ( $_POST [ 'pw' ] ) )
+  {
+
+
+ $conexion = mysql_connect ( $host , $user ) or die ("error al cargar los datos");
+
+   mysql_select_db( $db , $conexion ) or die ("error al cargar los datos");  
+	
+	mysql_query("INSERT INTO curso php (nombre,pw) value( '$_POST [ nombre ] ',' $_POST [ pw ] ' ) , $con " ) ;
+
+		echo "operacion realizada con exito";
+ }
+
+
+ ?>
