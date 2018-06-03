c798456d37c7a1b23530ffd91ecdedcdf228e152
Juan Guzmán <31047829+wh0am123@users.noreply.github.com>
2018-06-03 14:46:14 -0400

    Qlq
    Mamalo

--- db/insertar.php
+++ db/insertar.php
@@ -7,9 +7,9 @@ if ( isset ( $_POST [ 'nombre' ] ) && !empty ( $_POST [ 'nombre' ] ) &&
 
  $conexion = mysql_connect ( $host , $user ) or die ("error al cargar los datos");
 
-   mysql_select_db( $db , $conexion ) or die ("error al cargar los datos");  
+       mysql_select_db( $db , $conexion ) or die ("error al cargar los datos");  
 	
-	mysql_query("INSERT INTO curso php (nombre,pw) value( '$_POST [ nombre ] ',' $_POST [ pw ] ' ) , $con " ) ;
+	mysql_query("INSERT INTO curso php (nombre,pw) value(".$_POST [ nombre ]."','".$_POST [ pw ]."')", $con);
 
 		echo "operacion realizada con exito";
  }
